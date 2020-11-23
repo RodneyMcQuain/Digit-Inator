19497dae122cb8f9aed44defd57193853ae463a6
Rodney McQuain <rodneymcquain95@gmail.com>
2020-11-23 15:34:17 -0500

    wip

--- src/client/src/components/Drawing/DetectButton.tsx
+++ src/client/src/components/Drawing/DetectButton.tsx
@@ -36,8 +36,41 @@ const DetectButton = ({ canvasRef, setPredictions }: DetectButtonProps) => {
 const detectButtonHandler = async (canvas: HTMLCanvasElement, setPredictions: (predictions: number[]) => void) => {
     const ctx: CanvasRenderingContext2D = canvas.getContext('2d') as any;
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const predictions = await detect(imageData);
+
+    const predictions = await detect(autoTrimCanvas(imageData, canvas, ctx));
     setPredictions(predictions);
 };
+
+const autoTrimCanvas = (imageData: ImageData, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
+    let h = canvas.height;
+    let w = canvas.width;
+    const pix = { x: [] as number[], y: [] as number[] };
+    for (let y = 0; y < h; y++) {
+        for (let x = 0; x < w; x++) {
+            const index = (y * w + x) * 4;
+            if (imageData.data[index + 3] > 0) {
+                pix.x.push(x);
+                pix.y.push(y);
+            }
+        }
+    }
+    const sortFunction = (a: any, b: any): any => a - b;
+    pix.x.sort(sortFunction);
+    pix.y.sort(sortFunction);
+    var n = pix.x.length - 1;
+
+    const PADDING = 20;
+    // const max = Math.max(pix.x[n] - pix.x[0], pix.x[n] - pix.[0])
+    // const max = Math.max(pix.x[n] - pix.x[0], pix.y[n] - pix.y[0]);
+    // w = (PADDING * 2) + pix.x[n] - pix.x[0] + (pix.x[n] - pix.x[0] + max);
+    // h = (PADDING * 2) + pix.y[n] - pix.y[0] + (pix.y[n] - pix.y[0] + max);
+    w = (PADDING * 2) + pix.x[n] - pix.x[0];
+    h = (PADDING * 2) + pix.y[n] - pix.y[0];
+    return ctx.getImageData(pix.x[0] - PADDING, pix.y[0] - PADDING, w, h);
+
+    // canvas.width = w;
+    // canvas.height = h;
+    // ctx.putImageData(cut, 0, 0);
+}
 
 export default DetectButton;
