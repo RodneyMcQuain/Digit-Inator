b9f61185c9923c480f492b2a9e55f8446bb3a4e3
Vinamra Munot <munov01@pfw.edu>
2020-12-03 03:23:53 +0000

    Merged PR 73: fix: have unique aria labelled by id for everybody's detections elements
    - fix: have unique aria labelled by id for everybody's detections elements
    - clean: remove whitespace
    
    Related work items: #421

--- src/client/src/components/shared/DetectionCard.tsx
+++ src/client/src/components/shared/DetectionCard.tsx
@@ -5,23 +5,24 @@ import { Detection } from '../../types/Detection';
 import Card from './Card';
 import IconText from './IconText';
 import styles from '../../styles/components/shared/DetectionCard.module.scss';
+import { v4 as getUUID } from 'uuid';
 
 interface PredictionCardProps {
     detection: Detection;
 };
 
 const DetectionCard = ({ detection: { predictions, image, id } }: PredictionCardProps) => {
-    const predictionId = `detection-${id ?? 0}`;
+    const predictionId = `detection-${id ?? getUUID()}`;
     const prediction = getHighestPrediction(predictions);
     const predictionARIAText = `Prediction: ${prediction}`;
 
     return (
         <Card>
-            <img 
-                src={image} 
-                alt={`A drawing that I think is a ${prediction}`} 
+            <img
+                src={image}
+                alt={`A drawing that I think is a ${prediction}`}
                 aria-labelledby={predictionId}
-                className={styles['detected-image']} 
+                className={styles['detected-image']}
             />
             <p
                 title={predictionARIAText}
