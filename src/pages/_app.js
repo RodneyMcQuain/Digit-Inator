484692938dfe94ff35ad09b9cf173d8647436f4c
Rodney McQuain <rodneymcquain95@gmail.com>
2020-12-23 10:24:04 -0500

    feat: move head to app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/base/global.scss';
+import SEO from '../components/shared/SEO';
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => (
+    <>
+        <SEO />
+        <Component {...pageProps} />
+    </>
+);
 
 export default App;
